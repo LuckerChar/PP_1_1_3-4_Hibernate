e9e8ed0bf25c9019fb1f49e96f10b800a9902a67
LuckerChar <kokakolka85@gmail.com>
2022-06-29 18:55:31 +0300

    Hibernate123

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -11,9 +11,9 @@ public class Main {
         UserService userService = new UserServiceImpl();
         userService.createUsersTable();
 
-        userService.saveUser("Name1", "LastName1", (byte) 20);
-        userService.saveUser("Name2", "LastName2", (byte) 25);
-        userService.saveUser("Name3", "LastName3", (byte) 31);
+        userService.saveUser("Name1", "Last123124Name1", (byte) 20);
+        userService.saveUser("Name2", "Las1324tName2", (byte) 25);
+        userService.saveUser("Name3", "LastNhbghame3", (byte) 31);
         userService.saveUser("Name4", "LastName4", (byte) 38);
 
         userService.removeUserById(1);
