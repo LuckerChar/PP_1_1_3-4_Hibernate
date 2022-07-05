a4ce9a553decd2b1aff7c86ee768e033aef2be9a
LuckerChar <kokakolka85@gmail.com>
2022-07-05 10:51:10 +0300

    HibernateNew

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -11,25 +11,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-    //            try (
-//    Statement statement = connection.createStatement()) {
-//        statement.execute("CREATE TABLE IF NOT EXISTS users (" +
-//                "id bigint NOT NULL AUTO_INCREMENT," +
-//                "name varchar(255) DEFAULT NULL," +
-//                "lastName varchar(255) DEFAULT NULL ," +
-//                "age tinyint DEFAULT  NULL," +
-//                "PRIMARY KEY (id))");
-//    } catch (
-//    SQLException e) {
-//        e.printStackTrace();
-//    }
+    SessionFactory sessionFactory = Util.getUtil().getStatement();
     public UserDaoHibernateImpl() {
+
     }
 
 
     @Override
     public void createUsersTable() {
-        Session session = Util.getSessionfactory().openSession();
+        Session session = sessionFactory.openSession();
         try {
             session.beginTransaction();
             session.createSQLQuery("CREATE TABLE users (id INTEGER NOT NULL AUTO_INCREMENT, name VARCHAR(45), lastname VARCHAR(45), age BIGINT(3), PRIMARY KEY (id))").executeUpdate();
@@ -44,7 +34,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void dropUsersTable() {
-        Session session = Util.getSessionfactory().openSession();
+        Session session = sessionFactory.openSession();
         try {
             session.beginTransaction();
             String sql = "DROP TABLE if exists User";
@@ -61,7 +51,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        Session session = Util.getSessionfactory().openSession();
+        Session session = sessionFactory.openSession();
         try {
             session.beginTransaction();
             session.save(new User(name, lastName, age));
@@ -76,7 +66,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void removeUserById(long id) {
-        Session session = Util.getSessionfactory().openSession();
+        Session session = sessionFactory.openSession();
         try {
             session.beginTransaction();
             session.createQuery("DELETE User WHERE id = :userid").setParameter("userid", id).executeUpdate();
@@ -92,7 +82,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
-        Session session = Util.getSessionfactory().openSession();
+        Session session = sessionFactory.openSession();
         try {
             session.beginTransaction();
             users = session.createQuery("FROM User").list();
@@ -108,7 +98,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void cleanUsersTable() {
-        Session session = Util.getSessionfactory().openSession();
+        Session session = sessionFactory.openSession();
         try {
             session.beginTransaction();
             session.createSQLQuery("TRUNCATE TABLE users").executeUpdate();
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -15,53 +15,77 @@ import java.util.Properties;
 
 
 public final class Util {
-    // реализуйте настройку соеденения с БД
-    private static final String URL = "jdbc:mysql://localhost:3306/testdb";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static Util util;
+    public static Util getUtil() {
+        if (util == null) {
+            util = new Util();
+        }
+        return util;
+    }
+    public Util () {
+    }
+        private static final String URL = "jdbc:mysql://localhost:3306/testdb";
+        private static final String USER = "root";
+        private static final String PASSWORD = "root";
+        private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 
-    private static SessionFactory sessionfactory;
+        private static SessionFactory sessionfactory;
 
-    public static Connection getconnection() {
-        Connection connection = null;
+        public static Connection getconnection () {
+            Connection connection = null;
+            try {
+                Class.forName(DRIVER);
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            } catch (SQLException | ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            return connection;
+        }
+
+        public static SessionFactory getSessionfactory () {
+            if (sessionfactory == null) {
+                try {
+                    Configuration configuration = new Configuration();
+                    Properties settings = new Properties();
+                    settings.put(Environment.DRIVER, DRIVER);
+                    settings.put(Environment.URL, URL);
+                    settings.put(Environment.USER, USER);
+                    settings.put(Environment.PASS, PASSWORD);
+                    settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+                    settings.put(Environment.SHOW_SQL, "true");
+                    settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                    configuration.setProperties(settings);
+                    configuration.addAnnotatedClass(User.class);
+
+                    ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                            .applySettings(configuration.getProperties()).build();
+
+                    sessionfactory = configuration.buildSessionFactory(serviceRegistry);
+                } catch (Exception e) {
+                    System.out.println("Hibernate: Нет коннекта к базе");
+                    e.printStackTrace();
+                }
+            }
+            return sessionfactory;
+        }
+    public Connection getConnection() {
         try {
-            Class.forName(DRIVER);
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (SQLException | ClassNotFoundException e) {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        return connection;
     }
 
-    public static SessionFactory getSessionfactory() {
-        if (sessionfactory == null) {
-            try {
-                Configuration configuration = new Configuration();
-                Properties settings = new Properties();
-                settings.put(Environment.DRIVER, DRIVER);
-                settings.put(Environment.URL, URL);
-                settings.put(Environment.USER, USER);
-                settings.put(Environment.PASS, PASSWORD);
-                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
-                settings.put(Environment.SHOW_SQL, "true");
-                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
 
-                configuration.setProperties(settings);
-                configuration.addAnnotatedClass(User.class);
-
-                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                        .applySettings(configuration.getProperties()).build();
-
-                sessionfactory = configuration.buildSessionFactory(serviceRegistry);
-            } catch (Exception e) {
-                System.out.println("Hibernate: Нет коннекта к базе");
-                e.printStackTrace();
-            }
+    public SessionFactory getStatement() {
+        try {
+            return getConnection().createStatement();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-        return sessionfactory;
     }
-}
+    }
 
 //    private static final Connection CONNECTION = createConnection();
 //
